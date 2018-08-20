873606d312b88320560aae4402b59809a05a3f96
田田田田 <tianweigrace@qq.com>
2018-08-20 23:19:57 +0800

    update: using default get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogArticles
 
 # Create your views here.
@@ -8,6 +8,6 @@ def blog_title(request):
 
 
 def blog_article(request, article_id):
-    article = BlogArticles.objects.get(id=article_id)
+    article = get_object_or_404(BlogArticles, id=article_id)
     pub = article.publish
     return render(request, "blog/content.html", {"article": article, "publish": pub})
