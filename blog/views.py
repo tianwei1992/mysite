620432b503639e41f0cef45f28759bbe40ed01e5
田田田田 <tianweigrace@qq.com>
2018-08-20 23:03:10 +0800

    update: article content can be visited

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,9 @@ from .models import BlogArticles
 def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, "blog/titles.html", {"blogs":blogs})
+
+
+def blog_article(request, article_id):
+    article = BlogArticles.objects.get(id=article_id)
+    pub = article.publish
+    return render(request, "blog/content.html", {"article": article, "publish": pub})
