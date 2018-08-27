11119b769797037a38e86f569ac5084c789598fa
田田田田 <tianweigrace@qq.com>
2018-08-27 19:16:55 +0800

    load staticfiles from local rather than network

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from blog import views
 
 """patch request to corresponding function of view"""
+app_name = "blog"
 urlpatterns = [
     url('^$', views.blog_title, name="blog_title"),
     url(r'(?P<article_id>\d)/$', views.blog_article, name="blog_detail"),
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls import url, include
 
-app_name = 'blog'
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
