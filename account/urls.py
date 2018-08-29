09bf6a7684a634bea91d9dbfaac9b1c8e2188a2b
田田田田 <tianweigrace@qq.com>
2018-08-29 22:42:34 +0800

    route url with '/account/login' ro built-in login function-auth.as_view()

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from account import views
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
 """patch request to corresponding function of view"""
 app_name = "account"
 urlpatterns = [
-    url('^login/$', views.user_login, name="user_login"),
+    # url('^login/$', views.user_login, name="user_login"),
+    url(r'^login/$', auth_views.LoginView.as_view(), name="user_login"),
+    
 ]
