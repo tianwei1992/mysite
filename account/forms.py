85909fc49c88e40ff671d09954de164f3bbf1bef
grace <tianweigrace@qq.com>
2018-09-18 13:15:24 +0800

    update account/register,add two more fields with new model UserProfile

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -18,3 +19,8 @@ class RegistrationForm(forms.ModelForm):
         if cd['password'] != cd ['password2']:
             raise forms.ValidationError("Passwords do not match.")
         return cd['password2']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ("phone", "birth")
--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    """注册用新的model，通过user字段对User表一一对应"""
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    birth = models.DateField(blank=True, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    
+    def __str__(self):
+        return 'User {}'.format(self.user.username)
+
