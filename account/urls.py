a6ca7c717c50f3470e1d456c87b7b799c1808695
grace <tianweigrace@qq.com>
2018-09-19 23:02:01 +0800

    add new-password-change, including self-define htmls

--- account/urls.py
+++ account/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     url(r'^password-change/$', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')), name="password_change"),
     url(r'^password-change-done/$', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     
+    url(r'^new-password-change/$', auth_views.PasswordChangeView.as_view(template_name="account/password_change_form.html", success_url=reverse_lazy('account:password_change_done')), name="new_password_change"),
+    url(r'^new-password-change-done/$', auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"), name="new_password_change_done"),
 ]
