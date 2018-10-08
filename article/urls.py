2745cefbe048e90d2c868e1d7cd2824a794b325e
grace <tianweigrace@qq.com>
2018-10-08 21:50:19 +0800

    realize column-editing operation on page ./article/article-column with a pop-up

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^article-column/$', views.article_column, name="article_column"),
+    url(r'^rename-column/$', views.rename_article_column, name="rename_article_column"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 
 from .models import ArticleColumn
@@ -23,3 +24,18 @@ def article_column(request):
             ArticleColumn.objects.create(user=request.user, column=column_name)
             return HttpResponse('1')
 
+
+@login_required(login_url='/account/login/')
+@require_POST
+@csrf_exempt
+def rename_article_column(request):
+    column_name = request.POST["column_name"]
+    column_id = request.POST["column_id"]
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.column = column_name
+        line.save()
+        return HttpResponse("1")
+    except Exception as e:
+        print(e)
+        return HttpResponse("0")
