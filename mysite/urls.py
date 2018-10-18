d2ca3446e8130140d8d212e48aa7b8760df9400e
grace <tianweigrace@qq.com>
2018-10-18 16:41:59 +0800

    add a home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +24,6 @@ urlpatterns = [
     url(r'^account/', include('account.urls')),
     url(r'^pwd_reset/',include(('password_reset.urls', 'password_reset'), namespace='pwd_reset')),
     url(r'^article/',include(('article.urls', "article"), namespace='article')),
+    url(r'home/', TemplateView.as_view(template_name="home.html"), name="home"),
 
 ]
