8209134066e9bfb819a10aa8ecc283844c615162
grace <tianweigrace@qq.com>
2018-10-21 20:59:01 +0800

    realize articlesi in markdown with self-define module selector

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -2,6 +2,8 @@ from django import template
 register = template.Library()
 from article.models import ArticlePost
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 @register.simple_tag
 def total_articles():
@@ -19,3 +21,7 @@ def latest_articles(n=5):
 @register.simple_tag
 def most_commented_articles(n=3):
     return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by('-total_comments')[:n]
+
+@register.filter(name="markdown")
+def markdown_filter(text):
+    return mark_safe(markdown.markdown(text))
