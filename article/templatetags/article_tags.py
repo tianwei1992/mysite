db9d6ccf6d14874cd20deafa8d3b54319563c8f2
grace <tianweigrace@qq.com>
2018-10-21 12:00:41 +0800

    realize collecting the recent articles with self-define inclusion_tag

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -10,3 +10,7 @@ def total_articles():
 def author_total_articles(user):
     return user.article.count()
 
+@register.inclusion_tag('article/list/latest_articles.html')
+def latest_articles(n=5):
+    latest_articles = ArticlePost.objects.order_by("-created")[:n]
+    return {"latest_articles": latest_articles}
