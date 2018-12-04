59598d0b3c43f2c7ef6bd169c3e2b60450925e3a
grace <tianweigrace@qq.com>
2018-12-04 15:52:43 +0800

    add page:manage-course-list

--- course/urls.py
+++ course/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import AboutView, CourseListView,HisCourseListView 
+from .views import AboutView, CourseListView,HisCourseListView,ManageCourseListView 
 
 urlpatterns=[
     url(r'^about/$', AboutView.as_view(), name="about"),
     url(r'^course-list/$', CourseListView.as_view(), name="course_list"),
     url(r'^his-course-list/$', HisCourseListView.as_view(), name="his_course_list"),
+    url(r'^manage-course/$', ManageCourseListView.as_view(), name="manage_course"),
 ]
 
 
--- course/views.py
+++ course/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Course
 from django.views.generic import TemplateView,ListView
 from django.contrib.auth.models import User
+from braces.views import LoginRequiredMixin
 
 class AboutView(TemplateView):
     template_name="course/about.html"
@@ -12,13 +13,24 @@ class CourseListView(ListView):
     template_name = "course/course_list.html"
 
 
-class HisCourseListView(ListView):
-    #queryset=Course.objects.filter(user__in=User.objects.filter(username="tester1"))
+class UserMixin:
+    def get_queryset(self):
+        qs = super(UserMixin, self).get_queryset()
+        return qs.filter(user=self.request.user)
+    
+class UserCourseMixin(UserMixin, LoginRequiredMixin):
     model=Course
+    login_url = "/account/login/"
+
+class ManageCourseListView(UserCourseMixin, ListView):
+    context_object_name = "courses"
+    template_name = "course/manage/manage_course_list.html"
+
+class HisCourseListView(ListView):
     def get_queryset(self):
         qs = super(HisCourseListView, self).get_queryset()
         return qs.filter(user=User.objects.filter(username="tester1")[0])
-        # return qs.filter(user=User.objects.filter(username="tester1")[0])
+    model=Course
     context_object_name = "courses"
     template_name = "course/course_list.html"
     
