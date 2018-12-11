df544b6966e6f530d9d02143c1bfd9d7ef0ac3f1
grace <tianweigrace@qq.com>
2018-12-11 22:16:10 +0800

    add a unit test

--- course/tests.py
+++ course/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+import unittest
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
+from .models import Course, Lesson
 
-# Create your tests here.
+class BrandTest(TestCase):
+    def setUp(self):
+        # create a test user
+        user = User.objects.create(username='testuser', id=1)
+        user.set_password('12345')
+        user.save()
+
+        # create a course
+        course = Course.objects.create(title='couse1', id=3,overview='course1 overview', user_id=1)
+        course.save()
+
+        # create a lesson
+        lesson = Lesson.objects.create(title='lesson1', description='lesson1 description', user_id=1, course_id=3, attach=None)
+        lesson.save()
+
+        # Every test needs a client.
+        self.client = Client()
+        self.is_login = self.client.login(username='testuser', password='12345')
+
+    def test_details(self):
+        self.assertEqual(self.is_login,True)
+
+        # Issue a GET request.
+        response = self.client.get('/course/course-list/')
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Issue a POST request.
+        response = self.client.post('/course/lessons-list/3',{'course_id':3})
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Issue a GET request.
+        response = self.client.get('/course/lesson-detail/1')
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
--- mysite/settings.py
+++ mysite/settings.py
@@ -43,10 +43,11 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#    'django.contrib.sites',
+    'django.contrib.contenttypes',
     'blog',
     'music',
     "account",
